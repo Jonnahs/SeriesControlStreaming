0e942b97f816dc342e2b4d97cea6f225d31003ce
Jonnahs <jonas.desenvolvimento@gmail.com>
2021-04-18 12:46:05 -0300

    conexao com bd

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+use App\Models\Serie;
+
+class SeriesController extends Controller
+{
+    public function index() {
+        $series = Serie::all();
+        return view('series.index', compact('series'));
+    }
+    public function create() {
+        return view('series.create');
+    }
+    public function store(Request $request)
+    {
+        $serie = Serie::create($request->all());
+
+        echo "Série com id {$serie->id} criada: {$serie->nome}";
+        
+
+    }
+}
